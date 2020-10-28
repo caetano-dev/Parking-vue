25945c4f6f9babd560b49f4d5c2bd6c984e475c7
Pedro Pires <pcaetanop@gmail.com>
2020-10-27 21:05:18 -0300

    starting the price component

--- public/index.js
+++ public/index.js
@@ -1,50 +1,54 @@
 var CreateCar = new Vue({
-  // el: "#ParkingContainer",
   el: "#ParkingContainer",
   data: {
     CarNumber: "",
     price: 0,
     cars: [],
     prices: [],
-
   },
 
   methods: {
     AddPrice(){
-      // pay = this.price
-      const value = this.price.toString()
+      //remove last price from list
       if(this.prices.length > 0){
         this.prices.splice(0);
       }
       const currentDate = new Date();
+      const hour = currentDate.getHours();
+      const minutes = currentDate.getMinutes()
       const priceId = currentDate.getTime();
-
+      //fix this
+      const leaveTime = hour * 60 + minutes
+      
       this.prices.push({
         id: priceId,
-        pay: value,
+        // pay: this.price,
+        pay: leaveTime,
       });
     },
 
     // create car number
     AddCar() {
-      const FormatCar1 = this.CarNumber.substring(0, this.CarNumber.length - 4);
-      const FormatCar2 = this.CarNumber.slice(3);
-      const FinalCarNumber = FormatCar1 + "-" + FormatCar2;
+      //characters before and after "-"
+      const FirstCharacters = this.CarNumber.substring(0, this.CarNumber.length - 4);
+      const LastCharacters = this.CarNumber.slice(3);
+      const FinalCarNumber = FirstCharacters + "-" + LastCharacters;
       // create time
       const currentDate = new Date();
       const hour = currentDate.getHours();
       const minutes = currentDate.getMinutes() < 10 ? '0' + currentDate.getMinutes() : currentDate.getMinutes()
-      // : '' + currentDate.getMinutes() ;
-
-      const dateString = hour + ":" + minutes;
+      const calculateMinutes = currentDate.getMinutes()
+      //fix this
+      const arriveTime = hour * 60 + calculateMinutes
       // ID
       const carId = currentDate.getTime();
       if (this.CarNumber != "") {
         this.cars.push({
           id: carId,
-          time: dateString,
+          hour: hour,
+          minutes: minutes,
           title: FinalCarNumber.toUpperCase(),
-          // price: 
+          arriveTime: arriveTime,
         });
         this.CarNumber = "";
       }
@@ -55,6 +59,3 @@ var CreateCar = new Vue({
     },
   },
 });
-
-// v-bind:price="price"
-// v-bind:CarNumber="car"
